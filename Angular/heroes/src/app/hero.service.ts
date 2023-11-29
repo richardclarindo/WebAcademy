c565e4bb8bcfe08cece431cbd91625a685b93c95
Richard Clarindo <richard.clarindo@gmail.com>
2023-11-29 18:31:43 -0400

    Angular

---
+++ Angular/heroes/src/app/hero.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HEROES } from './heroes/mocks/heroes.mocks';
+import { Hero } from './Hero';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(private messageService: MessageService){}
+
+  getHeroesMock(): Observable<Hero[]> { 
+    const heroes = of(HEROES)
+    this.messageService.add("Hero Service: fetched heroes")
+    return heroes
+  }
+}
